3f8c0647a54084ee836a55ed0662d7271ef42706
luoawai <32483950+luoawai@users.noreply.github.com>
2021-03-07 23:40:12 +0800

    Update zhiyou.py

--- Zhiyou/zhiyou.py
+++ Zhiyou/zhiyou.py
@@ -10,8 +10,8 @@ def pusher(*args):
         othermsg += "\n"
     SCKEY = os.environ.get('SCKEY') # http://sc.ftqq.com/
     SCTKEY = os.environ.get('SCTKEY') # http://sct.ftqq.com/
-    Skey = os.environ.get('Skey') # https://cp.xuthus.cc/
-    Smode = os.environ.get('Smode') # send, group, psend, pgroup, wx, tg, ww, ding(no send email)
+    Skey = os.environ.get('SKEY') # https://cp.xuthus.cc/
+    Smode = os.environ.get('SMODE') # send, group, psend, pgroup, wx, tg, ww, ding(no send email)
     pushplus_token = os.environ.get('pushplus_token') # http://pushplus.hxtrip.com/
     pushplus_topic = os.environ.get('pushplus_topic') # pushplus一对多推送需要的"群组编码"，一对一推送不用管
     if SCKEY:
@@ -51,15 +51,14 @@ def pusher(*args):
         print(r.json())
         if r.json()["code"] != 200:
             print(f"pushplus推送失败！{r.json()['msg']}")
-    if Skey:
-           print(Skey)
-              if not Smode:
-                 Smode = 'send'
-              if othermsg:
-                 msg = msg + "\n" + othermsg
-              sendurl = f"https://push.xuthus.cc/{Smode}/{Skey}"
-              params = {"c" : msg}
-              requests.post(sendurl, params=params)
+   if Skey:
+        if not Smode:
+            Smode = 'send'
+        if othermsg:
+            msg = msg + "\n" + othermsg
+        sendurl = f"https://push.xuthus.cc/{Smode}/{Skey}"
+        params = {"c" : msg}
+        requests.post(sendurl, params=params)
 
 def run(*arg):
     msg = ""
